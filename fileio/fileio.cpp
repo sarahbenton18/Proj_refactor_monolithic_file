a7267b7dab0afddb1a13d2473afc3f350ef99f51
Sarah Benton <sarah.benton.18@cnu.edu>
2021-02-16 18:33:41 -0500

    took out using namespace std

--- fileio/fileio.cpp
+++ fileio/fileio.cpp
@@ -2,16 +2,15 @@
 #include <string>
 #include "../includes/fileio.h"
 #include "../includes/constants.h"
-using namespace std;
 
 //attempt to open file 'filename' and read in all data
 //returns SUCCESS if all goes well or COULD_NOT_OPEN_FILE
-int loadData(const std::string filename, vector<process> &myProcesses){
+int loadData(const std::string filename, std::vector<process> &myProcesses){
 	return UNIMPLEMENTED;
 }
 
 //attempt to create or open file 'filename' to write all data to
 //returns SUCCESS if all goes well or COULD_NOT_OPEN_FILE
-int saveData(const std::string filename, vector<process> &myProcesses){
+int saveData(const std::string filename, std::vector<process> &myProcesses){
 	return UNIMPLEMENTED;
 }
